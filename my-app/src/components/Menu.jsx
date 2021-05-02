41a7f418bda2591fcd1d3291b777c5fadfa769dc
Puja <pujasah2501@gmai.com>
2021-05-02 22:56:06 +0530

    initial

---
+++ my-app/src/components/Menu.jsx
@@ -0,0 +1,186 @@
+const Menu = [
+    {
+       id: 1,
+       image: "images/breadbutter.jpg",
+       name:"breadbutter",
+       category: "breakfast",
+       price: "30₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+ 
+   {
+       id: 2,
+       image: "images/conflex.jpg",
+       name:"conflex",
+       category: "breakfast",
+       price: "20₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+   {
+       id: 3,
+       image: "images/idli.jpg",
+       name:"idli",
+       category: "breakfast",
+       price: "30₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+    {
+       id: 4,
+       image: "../images/veg-thali.jpg",
+       name:"veg-thali",
+       category: "lunch",
+       price: "50₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+     {
+       id: 5,
+       image: "../images/uttapam.jpg",
+       name:"uttapam",
+       category: "evening",
+       price: "40₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+      {
+       id: 6,
+       image: "../images/chilli-chicken.jpg",
+       name:"chilli-chicken",
+       category: "dinner",
+       price: "180₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 7,
+       image: "../images/fried-rice.jpg",
+       name:"fried-rice",
+       category: "dinner",
+       price: "120₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 8,
+       image: "../images/sindhi-biryani.jpg",
+       name:"sindhi-biryani",
+       category: "lunch",
+       price: "160₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 9,
+       image: "../images/phuchka.jpg",
+       name:"phuchka",
+       category: "evening",
+       price: "80₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+
+   {
+    id: 10,
+    image: "../images/pasta.jpg",
+    name:"pasta",
+    category: "evening",
+    price: "180₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+{
+    id: 11,
+    image: "../images/puri.jpg",
+    name:"puri",
+    category: "breakfast",
+    price: "80₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+{
+    id: 12,
+    image: "../images/sandwich.jpg",
+    name:"sandwich",
+    category: "breakfast",
+    price: "80₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+{
+    id: 13,
+    image: "../images/pomfret-fish.jpg",
+    name:"pomfret-fish",
+    category: "dinner",
+    price: "180₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+{
+    id: 14,
+    image: "../images/veg-cutlet.jpg",
+    name:"veg-cutlet",
+    category: "evening",
+    price: "50₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+{
+    id: 15,
+    image: "../images/pyaz-pakore.jpg",
+    name:"pyaz-pakore",
+    category: "evening",
+    price: "40₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+
+{
+    id: 16,
+    image: "../images/paratha.jpg",
+    name:"paratha",
+    category: "dinner",
+    price: "80₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+
+{
+    id: 17,
+    image: "../images/noodles-cutlet.jpg",
+    name:"noodles-cutlet",
+    category: "evening",
+    price: "80₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+
+{
+    id: 18,
+    image: "../images/morog-polao.jpg",
+    name:"morog-polao",
+    category: "lunch",
+    price: "160₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+
+{
+    id: 19,
+    image: "../images/mutton-lemon-biryani.jpg",
+    name:"mutton-lemon-biryani",
+    category: "lunch",
+    price: "280₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+
+{
+    id: 20,
+    image: "../images/khichdi.jpg",
+    name:"khichdi",
+    category: "dinner",
+    price: "50₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+
+{
+    id: 21,
+    image: "../images/maggie.jpg",
+    name:"maggie",
+    category: "evening",
+    price: "80₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+{
+    id: 22,
+    image: "../images/fish-salon.jpg",
+    name:"fish-salon",
+    category: "lunch",
+    price: "180₹",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+},
+];
